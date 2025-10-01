b891fbaebd2760776afbbd404383618bf37f1614
AKotov-dev <alex_q_2000@mail.ru>
2025-10-01 13:28:12 +0300

    v3.7 + SDCard Manager Image Viewer (imagemagick)

--- adbmanager/backup/sdcardmanager.pas
+++ adbmanager/backup/sdcardmanager.pas
@@ -6,7 +6,7 @@ interface
 
 uses
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, StdCtrls,
-  ShellCtrls, ComCtrls, Buttons, IniPropStorage, Process, LCLType, Math;
+  ShellCtrls, ComCtrls, Buttons, IniPropStorage, Process, LCLType;
 
 type
 
